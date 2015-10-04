5d64ade8096f89a89606777d38e8d80cfcb6d363
Matthew Williams <mwilliamsnov6@gmail.com>
2015-10-04 14:30:13 -0700

    test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("ProfileApp", ['ngRoute']);
 app.config(['$routeProvider', '$httpProvider', '$locationProvider',  function($routeProvider, $httpProvider, $locationProvider) {
     // $httpProvider.defaults.withCredentials = true;
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: '/partials/home.html',
